10f61c2b8a88a42b93c2a1567d9cfdaf9f5fa53c
Braudin Laya <Braudinlaya@gmail.com>
2024-10-24 00:34:47 -0400

    feat: :sparkles: Avances en los diseños presentado en la demo

--- android/app/src/main/java/digital/identi/micacao/comercializa/MainActivity.kt
+++ android/app/src/main/java/digital/identi/micacao/comercializa/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.collectorapp
+package digital.identi.micacao.comercializa
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "collectorApp"
+  override fun getMainComponentName(): String = "MiCacao Comercializa"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
--- ios/MiCacaoComercializaTests/MiCacaoComercializaTests.m
+++ ios/MiCacaoComercializaTests/MiCacaoComercializaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface collectorAppTests : XCTestCase
+@interface MiCacaoComercializaTests : XCTestCase
 
 @end
 
-@implementation collectorAppTests
+@implementation MiCacaoComercializaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
